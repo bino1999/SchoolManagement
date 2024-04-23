4e4cddf47c5d98d18b4b71f8c2234d5e22a031b6
Binod Bandara <101051395+bino1999@users.noreply.github.com>
2024-04-23 08:23:11 +0530

    now
    check

--- CourseManagementService/eslint.config.mjs
+++ CourseManagementService/eslint.config.mjs
@@ -5,5 +5,10 @@ import pluginJs from "@eslint/js";
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
+  {env: {
+    browser: true,
+    es2021: true,
+    node: true,  // Add this line
+  }},
   pluginJs.configs.recommended,
 ];
