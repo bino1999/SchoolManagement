a6223178eae90120a965773f041142dcee6d5142
Binod Bandara <101051395+bino1999@users.noreply.github.com>
2024-05-11 14:51:06 +0530

    add
    all course

--- CourseManagementService/Controller/courseController.js
+++ CourseManagementService/Controller/courseController.js
@@ -14,7 +14,7 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
   try {
     const courses = await Course.find();
-    res.json(courses);
+    res.render('course', { courses: courses });
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
